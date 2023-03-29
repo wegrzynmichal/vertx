267524665deeb14a778fc6b464a90a88782ad795
Michał Węgrzyn <michal.wegrzyn@connectpoint.pl>
2023-03-29 13:37:48 +0200

    02: Improved HTTP server using Vert.x Router

--- src/main/java/pl/wegrzynm/vertx/MainVerticle.java
+++ src/main/java/pl/wegrzynm/vertx/MainVerticle.java
@@ -1,14 +1,21 @@
 package pl.wegrzynm.vertx;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response().end("hello world");
-        }).listen(8080);
+        Router router = Router.router(vertx);
+        router.get("/api/hello").handler(ctx -> {
+            ctx.request().response().end("Hello world");
+        });
+        router.get("/api/hello/:name").handler(ctx -> {
+            String name = ctx.pathParam("name");
+            ctx.request().response().end("Hello world " + name);
+        });
+        vertx.createHttpServer().requestHandler(router).listen(8081);
     }
 
 }
