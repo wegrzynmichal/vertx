e36b6fbaabd6d3c83d4c54ee9da4092a4c75bd4c
Michał Węgrzyn <michal.wegrzyn@connectpoint.pl>
2023-03-29 15:38:59 +0200

    12: Tested GitHub settings

--- src/main/java/pl/wegrzynm/vertx/MainVerticle.java
+++ src/main/java/pl/wegrzynm/vertx/MainVerticle.java
@@ -13,6 +13,7 @@ public class MainVerticle extends AbstractVerticle {
         router.get("/api/hello").handler(this::helloVertx);
         router.get("/api/hello/:name").handler(this::helloName);
         vertx.createHttpServer().requestHandler(router).listen(8081);
+
     }
 
     void helloVertx(RoutingContext ctx) {
