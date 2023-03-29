88eb5e5820ada24fcfd60bdcedc92bc5496d64b2
Michał Węgrzyn <michal.wegrzyn@connectpoint.pl>
2023-03-29 12:57:21 +0200

    initial commit

---
+++ src/main/java/pl/wegrzynm/vertx/MainVerticle.java
@@ -0,0 +1,12 @@
+package pl.wegrzynm.vertx;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
